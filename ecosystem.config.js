21bb8c0a27cb60b10884c4de6935ef92d7183a70
imseungkk <imseungkk@gmail.com>
2024-11-18 22:47:42 +0900

    Add PM2 ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,14 @@
 module.exports = {
   apps: [{
-    name: 'server',
+    name: 'ilovesales-backend',
     script: 'server.js',
     instances: 1,
     autorestart: true,
     watch: false,
     max_memory_restart: '1G',
     env: {
-      NODE_ENV: 'production'
+      NODE_ENV: 'production',
+      PORT: 3000
     }
   }]
 }; 
